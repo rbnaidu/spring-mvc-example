83c75397f554214ec6bcb33f9d321ec9640be7ae
raj <kvatanani@gmail.com>
2020-04-20 23:33:55 +0530

    added

---
+++ src/main/java/com/testdev/Testdevclass.java
@@ -0,0 +1,11 @@
+package com.testdev;
+
+public class Testdevclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=2;
+ System.out.println("hello"+i);
+	}
+
+}
