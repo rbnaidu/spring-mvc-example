5b94a0e779ad2b16bb337ebd9091af7d90230ae5
raj <kvatanani@gmail.com>
2020-04-20 23:42:03 +0530

    added 2nd

--- src/main/java/com/testdev/Testdevclass.java
+++ src/main/java/com/testdev/Testdevclass.java
@@ -5,7 +5,7 @@ public class Testdevclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=2;
- System.out.println("hello"+i);
+ System.out.println("hellos"+i);
 	}
 
 }
