14b607df7696612a8223c07b7deb278131d0daef
rbnaidu <kvatanani@gmail.com>
2021-02-22 13:23:41 +0530

    chanes for some wrong

--- src/main/java/com/testdev/Testdevclass.java
+++ src/main/java/com/testdev/Testdevclass.java
@@ -5,7 +5,7 @@ public class Testdevclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=2;
- System.out.println("hellos"+i);
+ System.out.println("hellos1"+i);
 	}
 
 }
